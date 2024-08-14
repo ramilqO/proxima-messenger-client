2beabf6346fa34bacff560800c7b3a8f8283f1a5
Ramil Osmanov <osmanovramil823@gmail.com>
2024-08-14 22:54:02 +0600

    Adding <AnimatePresence /> component for animations between pages

--- src/components/globals/Router/Router.tsx
+++ src/components/globals/Router/Router.tsx
@@ -1,41 +1,43 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { privateRoutes, publicRoutes } from './routes';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const isAuth = true;
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        {isAuth
-          ? privateRoutes.map(route => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.3 }}>{route.element}</motion.div>
-              }
-            />
-          ))
-          : publicRoutes.map(route => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.3 }}>{route.element}</motion.div>
-              }
-            />
-          ))}
-      </Routes>
+      <AnimatePresence>
+        <Routes>
+          {isAuth
+            ? privateRoutes.map(route => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.3 }}> {route.element}</motion.div>
+                }
+              />
+            ))
+            : publicRoutes.map(route => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.3 }}>{route.element}</motion.div>
+                }
+              />
+            ))}
+        </Routes>
+      </AnimatePresence>
     </BrowserRouter>
   );
 }
