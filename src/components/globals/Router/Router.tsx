d918196459df4c9553ccf64b566d667bb1a44d71
Ramil Osmanov <osmanovramil823@gmail.com>
2024-08-14 22:40:00 +0600

    animation between pages with framer-motion

--- src/components/globals/Router/Router.tsx
+++ src/components/globals/Router/Router.tsx
@@ -1,25 +1,41 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { privateRoutes, publicRoutes } from './routes';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { motion } from 'framer-motion';
 
 const isAuth = true;
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <TransitionGroup>
-        <CSSTransition in={true} timeout={300} classNames='page' unmountOnExit>
-          <Routes>
-            {isAuth
-              ? privateRoutes.map(route => {
-                  return <Route key={route.path} path={route.path} element={route.element} />;
-                })
-              : publicRoutes.map(route => {
-                  return <Route key={route.path} path={route.path} element={route.element} />;
-                })}
-          </Routes>
-        </CSSTransition>
-      </TransitionGroup>
+      <Routes>
+        {isAuth
+          ? privateRoutes.map(route => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}>{route.element}</motion.div>
+              }
+            />
+          ))
+          : publicRoutes.map(route => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}>{route.element}</motion.div>
+              }
+            />
+          ))}
+      </Routes>
     </BrowserRouter>
   );
 }
